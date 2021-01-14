a3175c3a68fe72ce93a44f9ed007bf7cafdeb9d2
Nacho Cougil <nacho.cougil@dynatrace.com>
2021-01-14 16:49:29 +0100

    Updated the data used from tests to solve the film repository test

--- src/test/java/com/cougil/FilmsObjectMother.java
+++ src/test/java/com/cougil/FilmsObjectMother.java
@@ -8,6 +8,6 @@ public class FilmsObjectMother {
             Arrays.asList("science-fiction", "thriller", "action"));
     public static final Film STAR_WARS_EPISODE_IV = new Film(45678,
             "Star Wars. Episode IV: A new hope", 1977,
-            Arrays.asList("space", "adventure", "galactic war", "droids", "rebellion"),
-            Arrays.asList("science-fiction", "adventures", "space", "fantasy"));
+            Arrays.asList("space", "galactic war", "droids", "rebellion"),
+            Arrays.asList("science-fiction", "adventures", "fantasy"));
 }
